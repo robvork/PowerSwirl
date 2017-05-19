921a97c446cd5963aaec60c5544dfb39854f7b3b
Rob Gevorkyan <robertgevorkyan@gmail.com>
2017-05-19 13:20:45 -0700

    Refresh 120PM

--- PSwirl Insert Filler Data.sql
+++ PSwirl Insert Filler Data.sql
@@ -7,15 +7,15 @@ CREATE PROCEDURE dbo.p_get_samples
 	/*
 		Table with following schema:
 		(
-			obj_type TINYINT NOT NULL -- the type of sample (corresponds to e.g. course, lesson, step)
-			obj_sid BIGINT NOT NULL -- the unique identifier of the object being sampled for
+			sample_type TINYINT NOT NULL -- the type of sample (corresponds to e.g. course, lesson, step)
+			sample_sid BIGINT NOT NULL -- the unique identifier of the sampleect being sampled for
 			min_val BIGINT NOT NULL -- the minimum value the sample can take
 			max_val BIGINT NOT NULL -- the maximum value the sample can take
 			sample_val BIGINT NULL -- the randomly sampled value (use seed if you want repeatability)
-			PRIMARY KEY(obj_type, obj_sid) -- each combination of obj_type and obj_sid gets a unique sample
+			PRIMARY KEY(sample_type, sample_sid) -- each combination of sample_type and sample_sid gets a unique sample
 		)
 	*/
-	, @ai_obj_type TINYINT
+	, @ai_sample_type TINYINT
 	, @ai_debug_level INT 
 )
 AS
@@ -28,7 +28,7 @@ BEGIN
 	(
 	 N'UPDATE ', @as_sample_table, N' 
 	   SET sample_val = min_val + (max_val - min_val) * RAND() + 1
-	   WHERE obj_type = ', @ai_obj_type, N'
+	   WHERE sample_type = ', @ai_sample_type, N'
 	   ;
 	  ' 
 	);
@@ -37,7 +37,7 @@ BEGIN
 
 	IF @ai_debug_level > 1
 	BEGIN
-		SET @ls_sql = CONCAT(N'SELECT obj_type, obj_sid, min_val, max_val, sample_val FROM ', @as_sample_table);
+		SET @ls_sql = CONCAT(N'SELECT sample_type, sample_sid, min_val, max_val, sample_val FROM ', @as_sample_table);
 		EXEC(@ls_sql);
 	END;
 END
@@ -53,43 +53,154 @@ CREATE PROCEDURE dbo.p_insert_filler_data
 ,	@ai_max_steps_per_lesson BIGINT
 ,	@ai_min_users BIGINT
 ,	@ai_max_users BIGINT
+,	@ai_debug_level INT
 )
 AS
 BEGIN
+	/******************************************************************************
+	Declare local variables
+	******************************************************************************/
+	DECLARE @li_sample_type_sid_course_count TINYINT;
+	DECLARE @li_sample_type_sid_lesson_count_by_course TINYINT;
+	DECLARE @li_sample_type_sid_step_count_by_lesson TINYINT;
+	DECLARE @li_sample_type_sid_user_count TINYINT;
+
+	DECLARE @ls_sample_table_name SYSNAME;
+	DECLARE @ls_curr_step NVARCHAR(100);
+
+	/******************************************************************************
+	Create temp tables
+	******************************************************************************/
+	BEGIN
+	--SET @ls_curr_step = N'Creating temp tables';
+
 	DROP TABLE IF EXISTS #sample; 
 
 	CREATE TABLE #sample
 	(
-		obj_type TINYINT NOT NULL
-	,	obj_sid BIGINT NOT NULL
+		sample_type_sid TINYINT NOT NULL
+	,	sample_sid BIGINT NOT NULL
 	,	min_val BIGINT NOT NULL
 	,	max_val BIGINT NOT NULL
 	,	sample_val BIGINT NULL
-	,	PRIMARY KEY(obj_type, obj_sid)
+	,	PRIMARY KEY(sample_type_sid, sample_sid)
 	);
+
+	SET @ls_sample_table_name = N'#sample';
 
 	DROP TABLE IF EXISTS #obj_type; 
 
-	CREATE TABLE #obj_type
+	CREATE TABLE #sample_type
 	(
-		obj_type TINYINT NOT NULL
-	,	obj_descr NVARCHAR(100) NOT NULL
-	,	PRIMARY KEY(obj_type)
+		sample_type TINYINT NOT NULL
+	,	sample_descr NVARCHAR(100) NOT NULL
+	,	PRIMARY KEY(sample_type)
 	)
 
-	CREATE TABLE #course_to_lesson_map
-	(
-		course_sid 
-	);
+	/*
+		Create temp tables with identical schemas to those found in the database
+	*/
+	BEGIN
+	SELECT TOP 0 * 
+	INTO #course_hdr 
+	FROM dbo.course_hdr;
 
-	INSERT INTO #obj_type
+	SELECT TOP 0 * 
+	INTO #course_dtl 
+	FROM dbo.course_dtl;
+
+	SELECT TOP 0 * 
+	INTO #lesson_hdr
+	FROM dbo.lesson_hdr;
+
+	SELECT TOP 0 * 
+	INTO #lesson_dtl 
+	FROM dbo.lesson_dtl;
+
+	SELECT TOP 0 * 
+	INTO #user_course
+	FROM dbo.user_course;
+
+	SELECT TOP 0 * 
+	INTO #user_hdr
+	FROM dbo.user_hdr;
+
+	SELECT TOP 0 * 
+	INTO #user_pause_state
+	FROM dbo.user_pause_state;
+	END
+	END
+
+	/******************************************************************************
+	Insert object types 
+	******************************************************************************/
+	-- The sample for an object with object type o is used in the creation of objects of that type
+
+	SET @li_sample_type_sid_course_count = 1;
+	SET @li_sample_type_sid_lesson_count_by_course = 2;
+	SET @li_sample_type_sid_step_count_by_lesson = 3;
+	SET @li_sample_type_sid_user_count = 4;
+
+	INSERT INTO #sample_type
 	VALUES
-	 (1, N'Course' )
-	,(2, N'Lesson' )
-	,(3, N'Step'   )
+	  (@li_sample_type_sid_course_count, N'Course Count')
+	, (@li_sample_type_sid_lesson_count_by_course, N'Lesson Count By Course')
+	, (@li_sample_type_sid_step_count_by_lesson, N'Step Count By Lesson')
+	, (@li_sample_type_sid_user_count, N'User Count')
+	;
+
+	/******************************************************************************
+	Sample number of courses
+	******************************************************************************/
+	INSERT INTO #sample
+	(
+		sample_type_sid
+	,	sample_sid
+	,	min_val
+	,	max_val
+	)
+	VALUES			   
+	(
+		@li_sample_type_sid_course_count
+	,	1
+	,	@ai_min_courses
+	,	@ai_max_courses
+	)
+	;
+
+	EXECUTE dbo.p_get_samples 
+		@as_sample_table = @ls_sample_table_name
+	,	@ai_sample_type = @li_sample_type_sid_course_count
+	,   @ai_debug_level = @ai_debug_level
+	;
+
+	/******************************************************************************
+	Create sampled number of courses
+	******************************************************************************/
+	INSERT INTO #course_hdr(course_sid, course_id)
+	SELECT n, CONCAT(N'C', n)
+	FROM dbo.GetNums
+	(
+	   1
+	,  (
+		SELECT sample_val 
+		FROM #sample 
+		WHERE sample_type_sid = @li_sample_type_sid_course_count
+	   )
+	)
+
+	IF @ai_debug_level > 1
+	BEGIN
+		SELECT '#course_hdr';
+		SELECT * FROM #course_hdr;
+	END;
 
 
-
+	/*
+		Sample number of courses C with min_C and max_C
+		For each course, sample a number of lessons L with min_L and max_L
+		For each lesson, sample a number of steps S with min_S and max_S
+	*/
 END;	
 
 
