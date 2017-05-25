4d2fff8781101b57e85249405250cccb2b5afa3f
Rob Gevorkyan <robertgevorkyan@gmail.com>
2017-05-25 00:37:37 -0700

    Refresh 1230AM

--- PS_Functions/courseLessonSelection.ps1
+++ PS_Functions/courseLessonSelection.ps1
@@ -28,32 +28,13 @@ function Write-CourseHeaders
     [CmdletBinding()]
     param
     (
-        $Courses
+        [CourseHeader[]] $CourseHeaders
     )
 
     if($Courses -eq $null)
     {
         throw "Courses must be non-null"
     }
-
-    $requiredProperties = "selection","course_id"
-    $actualProperties = Get-Member -InputObject $Courses -MemberType Property 
-    $hasRequiredProperties = $true 
-    $missingProperties = [string[]]@()
-    foreach($rp in $requiredProperties)
-    {
-        if($rp -notin $actualProperties)
-        {
-            $hasrequiredProperties = $false
-            $missingProperties += $rp
-        }
-    }
-    if(-not $hasRequiredProperties)
-    {
-        throw "Courses does not have the required properties. Missing propeties : $($missingProperties -join ",")"
-    }
-
-    
 
     $CourseCount = $Courses | Measure-Object | Select-Object -ExpandProperty Count
     Write-Information -MessageData $CourseCount -Tags CourseCount -InformationAction SilentlyContinue
@@ -147,11 +128,25 @@ function Test-MenuSelection
     ,   [int] $MenuSelection
     )
 
-    $MenuSelections = $MenuObjects.Selection
+    $MenuSelections = $MenuObjects.selection
 
     if($MenuSelection -notin $MenuSelections)
     {
         throw "$MenuSelection is not a valid selection"
+    }
+}
+
+class CourseHeader
+{
+    [Int] $Selection 
+    [String] $CourseID 
+    [Int] $CourseSID 
+
+    CourseHeader([Int]$Selection, [String] $CourseID, [Int] $CourseSID)
+    {
+        $this.Selection = $Selection
+        $this.CourseID = $CourseID
+        $this.CourseSID = $CourseSID 
     }
 }
 
@@ -178,9 +173,35 @@ function Get-CourseHeaders
     $Query = "EXEC dbo.p_get_courses"
     $Courses = Invoke-SqlCmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query 
 
-    Write-Output $Courses 
+    Test-HasNoDBNulls $Courses "selection"
+    Test-HasNoDBNulls $Courses "course_id"
+    Test-HasNoDBNulls $Courses "course_sid"
 
+    Test-HasNoEmptyStrings $Courses "course_id"
 
+    Test-HasNoDuplicates $Courses "selection"
+    Test-HasNoDuplicates $Courses "course_id"
+    Test-HasNoDuplicates $Courses "course_sid"
+
+    foreach($c in $Courses)
+    {
+        $courseHeader = [CourseHeader]::new($c.selection, $c.course_id, $c.course_sid)
+        Write-Output $courseHeader
+    }
+}
+
+class LessonHeader
+{
+    [Int] $Selection 
+    [String] $LessonID 
+    [Int] $LessonSID 
+
+    CourseHeader([Int]$Selection, [String] $LessonID, [Int] $LessonSID)
+    {
+        $this.Selection = $Selection
+        $this.LessonID = $LessonID
+        $this.LessonSID = $LessonSID 
+    }
 }
 
 function Get-LessonHeaders
@@ -213,7 +234,104 @@ function Get-LessonHeaders
     )
     
     $Query = "EXECUTE dbo.p_get_lessons @ai_course_sid = $CourseSid"
-    $LessonHeaders = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query 
-    Write-Output $LessonHeaders 
+    $Lessons = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query 
+    foreach($l in $Lessons)
+    {
+        $LessonHeader = [LessonHeader]::new($l.selection, $l.lesson_id, $l.lesson_sid)
+        Write-Output $LessonHeader
+    }
 
+}
+
+function Test-HasNoDBNulls
+{
+    param
+    (
+        [Object[]] $Objects
+    ,
+        [String] $Property
+    )
+
+    if($Objects -eq $null -or $Property -eq $null)
+    {
+        throw "Objects and property must be not null"
+    }
+
+    if($Property -eq "")
+    {
+        throw "Property must be non-empty"
+    }
+
+    if($Property -notin ($Objects | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name))
+    {
+        throw "Property must be a property of the objects"
+    }
+
+    if($Objects[$Property] | Where-Object {$_ -is [System.DBNull]})
+    {
+        throw "Values of '$Property' must be non-null"
+    }   
+}
+
+function Test-HasNoDuplicates
+{
+    param
+    (
+        [Object[]] $Objects
+    ,
+        [String] $Property
+    )
+
+    if($Objects -eq $null -or $Property -eq $null)
+    {
+        throw "Objects and property must be not null"
+    }
+
+    if($Property -eq "")
+    {
+        throw "Property must be non-empty"
+    }
+
+    if($Property -notin ($Objects | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name))
+    {
+        throw "Property must be a property of the objects"
+    }
+
+    $Count = $Objects.Length 
+    $UniqueCount = $Objects[$Property] | 
+                   Sort-Object -Unique 
+    if($Count -ne $UniqueCount)
+    {
+        throw "Values of '$Property' must be unique"
+    }   
+}
+
+function Test-HasNoEmptyStrings
+{
+    param
+    (
+        [Object[]] $Objects
+    ,
+        [String] $Property
+    )
+
+    if($Objects -eq $null -or $Property -eq $null)
+    {
+        throw "Objects and property must be not null"
+    }
+
+    if($Property -eq "")
+    {
+        throw "Property must be non-empty"
+    }
+
+    if($Property -notin ($Objects | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name))
+    {
+        throw "Property must be a property of the objects"
+    }
+
+    if($Objects[$Property] -contains "")
+    {
+        throw "Values of '$Property' must be non-empty"
+    }
 }
