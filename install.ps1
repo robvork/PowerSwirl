1c1718de69e22b75c92c16ca4687440042eba80e
Rob Gevorkyan <robertgevorkyan@gmail.com>
2017-07-16 21:48:07 -0700

    Refresh 950PM

---
+++ install.ps1
@@ -0,0 +1,37 @@
+﻿[CmdletBinding()]
+param 
+(
+    [Paramater(Mandatory=$true)]
+    $ServerInstance
+,   
+    [Paramater(Mandatory=$true)]
+    $Database = "PowerSwirl"
+,   
+    [Paramater(Mandatory=$true)]
+    $ModuleRoot 
+)
+
+Import-Module PowerSwirl -Force 
+
+$InstallScripts = @{
+        DataTypesPath = "$ModuleRoot\Database\Data Types";
+        TablesPath = "$ModuleRoot\Database\Tables";
+        ConstraintsPath = "$ModuleRoot\Database\Constraints";
+        FunctionsPath = "$ModuleRoot\Database\Functions";
+        ProceduresPath = "$ModuleRoot\Database\Procedures";
+        ViewsPath = "$ModuleRoot\Database\Views";
+        TriggersPath = "$ModuleRoot\Database\Triggers";
+    }
+
+Install-PowerSwirl -Force -ErrorAction Stop @InstallScripts -ServerInstance $ServerInstance -Database $Database -Verbose 
+
+$LessonMarkupPaths = @(
+".\database\Data\Lessons\PowerShell Orientation\using_the_help_system.xml"
+".\database\Data\Lessons\PowerShell Orientation\wildcards.xml"
+)
+
+$LessonMarkupPaths | 
+Select-Object @{n="Path"; e={$_}} | 
+Get-Content -Raw | 
+ConvertFrom-PowerSwirlLessonMarkup | 
+Import-PowerSwirlLesson -CreateNewCourse -OverWriteLesson -Verbose 
